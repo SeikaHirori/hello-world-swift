6842c3af5ee2cafb2d1af01c54c7b0f519144068
WaToArt <93100367+WaToArt@users.noreply.github.com>
2023-01-16 12:58:01 -0800

    Initial Commit

---
+++ hello world swift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  hello world swift
+//
+//  Created by Rich Q on 1/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello world swift/hello_world_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_world_swiftApp.swift
+//  hello world swift
+//
+//  Created by Rich Q on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct hello_world_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
